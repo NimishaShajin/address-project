52a9f40b2af6e1e43442472cdadf83f081343105
Nimisha.Shajin <nshajin@gosi.gov.sa>
2021-11-10 20:49:16 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,28 +9,13 @@ export class AppComponent {
   value: any;
   title = 'address-project';
   test = 'test code';
-  name?: string;
-  age: string = "";
-  gender : string = "";
-  addr1 : string = "";
-  addr2 : string = "";
-  city : string = "";
-  pincode : string = "";
-  state : string = "";
-  country : string = "";
+  formData : any;
   showFinalResult : boolean= false;
 
   showChildComponent(e: any) {
     console.log(e);
-    this.showFinalResult = true;
-    this.name = e.nameForm;
-    this.age = e.ageForm;
-    this.gender = e.genderForm;
-    this.addr1 = e.address1;
-    this.addr2 = e.address2;
-    this.pincode = e.pincode;
-    this.city = e.city;
-    this.state = e.state;
-    this.country = e.country;
+    this.formData = e;
+    this.showFinalResult = true;    
+    
   }
 }
