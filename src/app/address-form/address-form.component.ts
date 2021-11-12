935a0927d9619f89dfdb795a9c409566c477a8a8
Nimisha.Shajin <nshajin@gosi.gov.sa>
2021-11-12 21:03:00 +0530

    updated

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -1,33 +1,39 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators ,FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-address-form',
   templateUrl: './address-form.component.html',
   styleUrls: ['./address-form.component.css']
 })
-export class AddressFormComponent {
-  formInstance: FormGroup;
+export class AddressFormComponent implements OnInit{
+  formInstance: any;
   @Output() newItemEvent = new EventEmitter();
+ 
 
-  constructor() {
-    this.formInstance = new FormGroup({
-      nameForm: new FormControl('', Validators.required),
-      ageForm: new FormControl('', Validators.required),
-      genderForm: new FormControl(''),
-      address1: new FormControl(''),
-      address2: new FormControl(''),
-      city: new FormControl(''),
-      pincode: new FormControl(''),
-      state: new FormControl(''),
-      country: new FormControl(''),
-   })
+  constructor(public form: FormBuilder) {}
+  ngOnInit(): void {
+    this.buildForm();
+  }
+  
+  buildForm(){
+    this.formInstance = this.form.group({
+        nameForm: [''],
+        ageForm: [''],
+        genderForm: [''],
+        address1: [''],
+        address2: [''],
+        city: [''],
+        pincode: [''],
+        state: [''],
+        country: ['']
+     })
   }
 
  save() { 
-   
+
    this.newItemEvent.emit(this.formInstance.value);
-   
+
  }
 
 }
